0d04f2ae052e5149c57f45b0a23bba75744754b9
Dario Mercurio <mercurio.dario@gmail.com>
2025-12-05 10:50:48 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,19 +5,24 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
+        // **********************************************
+        // ➡️ AGGIUNGI QUI LA PROPRIETÀ 'base'
+        // Deve corrispondere al nome del tuo repository preceduto e seguito da '/'
+        base: '/workflow.app/', 
+        // **********************************************
+        server: {
+            port: 3000,
+            host: '0.0.0.0',
+        },
+        plugins: [react()],
+        define: {
+            'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+            'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, '.'),
+            }
         }
-      }
     };
 });
